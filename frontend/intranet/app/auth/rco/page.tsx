d9bcc23bc85fed0b527df81ab51585e644b79090
Marcelo Silva <marcelo.silva070899@gmail.com>
2026-08-13 14:01:45 -0300

    feat aparencia rco

--- frontend/intranet/app/auth/rco/page.tsx
+++ frontend/intranet/app/auth/rco/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { FaCalculator} from "react-icons/fa";
+import { FaCalculator } from "react-icons/fa";
 import { RcoForm } from "@/components/rco-form/rco-from";
 import BackButton from "@/components/back-button/back-button";
 
